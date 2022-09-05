8e02de67ee4eeab0a8dfdc79d369238ef000f0b4
Donizete Vida <donizetevida1998@outlook.com>
2022-09-04 23:25:35 -0300

    Simple websocket message interface

--- src/main/kotlin/com/fatec/tcc/animais/chatbot/ChatbotController.kt
+++ src/main/kotlin/com/fatec/tcc/animais/chatbot/ChatbotController.kt
@@ -1,12 +1,14 @@
 package com.fatec.tcc.animais.chatbot
 
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.messaging.handler.annotation.MessageMapping
+import org.springframework.messaging.handler.annotation.SendTo
+import org.springframework.stereotype.Controller
 
-@RestController
-@RequestMapping("/chatbot")
-@SecurityRequirement(name = "jwt")
-class ChatbotController(
-    private val chatbot: Chatbot
-)
+@Controller
+class ChatbotController {
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    fun greetings(message: String): String {
+        return "I received you message: $message"
+    }
+}
---
+++ src/main/kotlin/com/fatec/tcc/animais/chatbot/WebSocketMessageBrokerConfig.kt
@@ -0,0 +1,20 @@
+package com.fatec.tcc.animais.chatbot
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
+
+@Configuration
+@EnableWebSocketMessageBroker
+class WebSocketMessageBrokerConfig : WebSocketMessageBrokerConfigurer {
+    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+        registry.addEndpoint("/ws");
+    }
+
+    override fun configureMessageBroker(registry: MessageBrokerRegistry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topic");
+    }
+}
