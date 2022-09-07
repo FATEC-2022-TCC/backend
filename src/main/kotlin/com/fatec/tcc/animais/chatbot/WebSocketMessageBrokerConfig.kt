6eea7aca02fec8843673e1fa164550983fc40b19
Donizete Vida <donizetevida1998@outlook.com>
2022-09-07 18:03:19 -0300

    Trying to add session management using redis implementation

--- src/main/kotlin/com/fatec/tcc/animais/chatbot/WebSocketMessageBrokerConfig.kt
+++ src/main/kotlin/com/fatec/tcc/animais/chatbot/WebSocketMessageBrokerConfig.kt
@@ -2,14 +2,17 @@ package com.fatec.tcc.animais.chatbot
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.session.Session
+import org.springframework.session.web.socket.config.annotation.AbstractSessionWebSocketMessageBrokerConfigurer
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
-class WebSocketMessageBrokerConfig : WebSocketMessageBrokerConfigurer {
-    override fun registerStompEndpoints(registry: StompEndpointRegistry) {
+@EnableScheduling
+class WebSocketMessageBrokerConfig : AbstractSessionWebSocketMessageBrokerConfigurer<Session>() {
+    override fun configureStompEndpoints(registry: StompEndpointRegistry) {
         registry.addEndpoint("/ws")
     }
 
