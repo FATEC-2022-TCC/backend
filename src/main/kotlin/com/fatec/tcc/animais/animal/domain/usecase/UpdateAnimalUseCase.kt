e0103830bfec6e415d5d7b4db0443351ad281d83
Johnny <Johnnylsrdarocha@gmail.com>
2022-09-30 20:30:06 -0300

    retorno do animal, no update.

--- src/main/kotlin/com/fatec/tcc/animais/animal/domain/usecase/UpdateAnimalUseCase.kt
+++ src/main/kotlin/com/fatec/tcc/animais/animal/domain/usecase/UpdateAnimalUseCase.kt
@@ -6,6 +6,7 @@ import com.fatec.tcc.animais.animal.domain.repository.AnimalRepository
 import com.fatec.tcc.animais.base.ErrorCode
 import com.fatec.tcc.animais.base.error
 import com.fatec.tcc.animais.base.Result
+import com.fatec.tcc.animais.base.success
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 
@@ -15,15 +16,14 @@ class UpdateAnimalUseCase(
 ) {
    operator fun invoke(id: Long, updateAnimalRequest: UpdateAnimalRequest): ResponseEntity<Result<Animal>>{
        val animal = animalRepository.find(id)  ?: return "Animal id not found" error ErrorCode.NOT_FOUND
-       animalRepository.update(
+       return animalRepository.update(
            animal.copy(
                name = updateAnimalRequest.name,
                description = updateAnimalRequest.description,
                type = updateAnimalRequest.type,
                birth = updateAnimalRequest.birth
            )
-       )
-       return ResponseEntity.ok().build()
+       ).success()
    }
 
 }
