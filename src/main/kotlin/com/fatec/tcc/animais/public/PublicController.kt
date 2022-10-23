83e49294ffa69959727156bc04076aa7110ce024
Donizete Vida <donizetevida1998@outlook.com>
2022-10-23 02:54:37 -0300

    Fix public controller cross origin shit

--- src/main/kotlin/com/fatec/tcc/animais/public/PublicController.kt
+++ src/main/kotlin/com/fatec/tcc/animais/public/PublicController.kt
@@ -2,14 +2,13 @@ package com.fatec.tcc.animais.public
 
 import com.fatec.tcc.animais.content.domain.usecase.GetContentProjectionInEvidenceUseCase
 import com.fatec.tcc.animais.services.file.PublicFileService
-import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import java.util.*
 
+@CrossOrigin
 @RestController
-@SecurityRequirement(name = "jwt")
 @RequestMapping("/public")
 class PublicController(
     private val fileService: PublicFileService,
