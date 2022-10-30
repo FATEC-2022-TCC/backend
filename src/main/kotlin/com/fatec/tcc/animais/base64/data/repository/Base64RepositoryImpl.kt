8cd22d3fa15ac083ad020a37238aa98efa1152e6
Donizete Vida <donizetevida1998@outlook.com>
2022-10-30 04:01:48 -0300

    Base64 feature was refactored

--- src/main/kotlin/com/fatec/tcc/animais/base64/data/repository/Base64RepositoryImpl.kt
+++ src/main/kotlin/com/fatec/tcc/animais/base64/data/repository/Base64RepositoryImpl.kt
@@ -1,12 +1,17 @@
 package com.fatec.tcc.animais.base64.data.repository
 
+import com.fatec.tcc.animais.base.DefaultRepository
+import com.fatec.tcc.animais.base64.data.entity.Base64Entity
 import com.fatec.tcc.animais.base64.data.entity.Base64EntityRepository
-import com.fatec.tcc.animais.base64.domain.repository.Base64Repository
+import com.fatec.tcc.animais.base64.data.mapper.Base64Mapper
+import com.fatec.tcc.animais.base64.domain.model.Base64
 import org.springframework.stereotype.Repository
 
 @Repository
 internal class Base64RepositoryImpl(
-    private val base64EntityRepository: Base64EntityRepository
-) : Base64Repository {
-    override fun delete(id: Long) = base64EntityRepository.deleteById(id)
-}
+    base64EntityRepository: Base64EntityRepository,
+    base64Mapper: Base64Mapper
+) : DefaultRepository<Base64, Base64Entity, Base64EntityRepository>(
+    repository = base64EntityRepository,
+    repositoryMapper = base64Mapper
+)
--- src/main/kotlin/com/fatec/tcc/animais/base64/domain/repository/Base64Repository.kt
+++
@@ -1,5 +0,0 @@
-package com.fatec.tcc.animais.base64.domain.repository
-
-interface Base64Repository {
-    fun delete(id: Long)
-}
