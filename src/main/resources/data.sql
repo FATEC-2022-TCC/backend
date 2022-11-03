77b93a4bdae61de3c5d23e150d22e309c1c86049
Donizete Vida <donizetevida1998@outlook.com>
2022-11-03 00:52:29 -0300

    I'm getting blinded (by the lights?)

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,10 +2,10 @@ INSERT INTO users (name, username, password, email, authority, created, created_
 VALUE ("SYSTEM", "SYSTEM", "$2a$10$YFn5pXKY/ktIlIkCQ2OpmehUMriXHjdoJ12GWDyjKGT6cpa4Dhv4y", "system@system.com", "ADMIN", NOW(), "SYSTEM", NOW(), "SYSTEM");
 
 INSERT INTO users (name, username, password, email, authority, created, created_by, updated, updated_by)
-VALUE ("anonymousUser", "anonymousUser", "$2a$10$YFn5pXKY/ktIlIkCQ2OpmehUMriXHjdoJ12GWDyjKGT6cpa4Dhv4y", "system@system.com", "ADMIN", NOW(), "SYSTEM", NOW(), "SYSTEM");
+VALUE ("anonymousUser", "anonymousUser", "$2a$10$YFn5pXKY/ktIlIkCQ2OpmehUMriXHjdoJ12GWDyjKGT6cpa4Dhv4y", "anonymousUser@anonymousUser.com", "", NOW(), "SYSTEM", NOW(), "SYSTEM");
 
 INSERT INTO users (name, username, password, email, authority, created, created_by, updated, updated_by)
-VALUE ("admin", "admin", "$2a$10$YFn5pXKY/ktIlIkCQ2OpmehUMriXHjdoJ12GWDyjKGT6cpa4Dhv4y", "anonymousUser@anonymousUser.com", "", NOW(), "SYSTEM", NOW(), "SYSTEM");
+VALUE ("admin", "admin", "$2a$10$YFn5pXKY/ktIlIkCQ2OpmehUMriXHjdoJ12GWDyjKGT6cpa4Dhv4y", "admin@admin.com", "ADMIN", NOW(), "SYSTEM", NOW(), "SYSTEM");
 
 INSERT INTO users (name, username, password, email, authority, created, created_by, updated, updated_by)
 VALUE ("test", "test", "$2a$10$LI/suG/VrLPuBQDRVhfG7.ntbrTV7IC7A9iSOFwEJH/t7DZaOuBk.", "test@test.com", "NORMAL", NOW(), "SYSTEM", NOW(), "SYSTEM");
