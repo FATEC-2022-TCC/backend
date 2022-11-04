da7b655009dcc02f2ece0b5fe5ab8cb3b1140789
Donizete Vida <donizetevida1998@outlook.com>
2022-11-04 04:01:22 -0300

    Fix index access by using code itself

--- src/main/kotlin/com/fatec/tcc/animais/complaint/domain/constant/ComplaintStatus.kt
+++ src/main/kotlin/com/fatec/tcc/animais/complaint/domain/constant/ComplaintStatus.kt
@@ -4,10 +4,10 @@ enum class ComplaintStatus(
     val code: Int,
     val description: String
 ) {
-    CREATED(1, "A denúncia foi criada"),
-    RUNNING(2, "A denúncia está sendo apurada"),
-    PAUSED(3, "A denúncia está pausada"),
-    RESOLVED(4, "A denúncia foi resolvida");
+    CREATED(0, "A denúncia foi criada"),
+    RUNNING(1, "A denúncia está sendo apurada"),
+    PAUSED(2, "A denúncia está pausada"),
+    RESOLVED(3, "A denúncia foi resolvida");
 
     companion object {
         operator fun get(index: Int) = values().let { if (index < 0 || index >= it.size) null else it[index] }
