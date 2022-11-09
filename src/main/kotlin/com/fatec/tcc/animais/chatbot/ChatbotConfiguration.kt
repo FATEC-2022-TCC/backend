a86f7306543c9ffef9cd717160cd26902f1fd5e7
Donizete Vida <donizetevida1998@outlook.com>
2022-11-09 05:08:04 -0300

    Some code refactor.
    Trying to achieve contextual nodes response feature.

--- src/main/kotlin/com/fatec/tcc/animais/chatbot/ChatbotConfiguration.kt
+++ src/main/kotlin/com/fatec/tcc/animais/chatbot/ChatbotConfiguration.kt
@@ -1,12 +1,13 @@
 package com.fatec.tcc.animais.chatbot
 
-import base.processor.NodeManager
+import base.DefaultNodeManager
+import base.DefaultNodeManagerBuilder
 import extension.use
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.util.ResourceUtils
 import parser.Parser
-import parser.json.Aiml
+import parser.json.Knowledge
 import parser.json.JSONParseImpl
 import java.io.FileInputStream
 import java.util.function.Supplier
@@ -14,19 +15,20 @@ import java.util.function.Supplier
 @Component
 class ChatbotConfiguration {
     class ChatbotImpl(
-        private val nodeManager: NodeManager
+        private val defaultNodeManager: DefaultNodeManager
     ) : Chatbot {
-        override fun invoke(message: String) = nodeManager.find(message)
+        override fun invoke(message: String) = defaultNodeManager.find(message)
     }
 
     @Bean
     fun providesChatbotSupplier(): Supplier<Chatbot> = object : Supplier<Chatbot> {
-        val parser: Parser<Aiml> = JSONParseImpl(Aiml::class)
+        val parser: Parser<Knowledge> = JSONParseImpl(Knowledge::class)
 
         val files = ResourceUtils.getFile("classpath:bots/joselito/").listFiles() ?: emptyArray()
         val data = files.map(::FileInputStream).use(parser::parse)
-        val nodeManager = NodeManager.build(data)
 
-        override fun get() = nodeManager.get().run(::ChatbotImpl)
+        val defaultNodeManager = DefaultNodeManagerBuilder.build(data)
+
+        override fun get() = defaultNodeManager.get().run(::ChatbotImpl)
     }
 }
