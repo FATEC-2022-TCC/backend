e1b3cd02b5df6597c4c27b4f5d7cc54870545395
Donizete Vida <donizetevida1998@outlook.com>
2022-11-17 07:33:23 -0300

    Home page refactor

--- src/main/kotlin/com/fatec/tcc/animais/public/PublicContentController.kt
+++ src/main/kotlin/com/fatec/tcc/animais/public/PublicContentController.kt
@@ -2,6 +2,7 @@ package com.fatec.tcc.animais.public
 
 import com.fatec.tcc.animais.content.domain.usecase.GetContentProjectionInEvidenceUseCase
 import com.fatec.tcc.animais.content.domain.usecase.GetContentUseCase
+import com.fatec.tcc.animais.content.domain.usecase.SearchContentProjectionUseCase
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
@@ -10,10 +11,20 @@ import java.util.*
 @RequestMapping("/public/content")
 class PublicContentController(
     private val getContentProjectionInEvidenceUseCase: GetContentProjectionInEvidenceUseCase,
-    private val getContentUseCase: GetContentUseCase
+    private val getContentUseCase: GetContentUseCase,
+    private val searchContentProjectionUseCase: SearchContentProjectionUseCase
 ) {
     @GetMapping("/projection/evidence")
     fun projectionInEvidence() = getContentProjectionInEvidenceUseCase()
+
+    @GetMapping("/projection")
+    fun project(
+        @RequestParam(defaultValue = "") text: String,
+        @RequestParam(defaultValue = "1") page: Int,
+    ) = searchContentProjectionUseCase(
+        text,
+        page - 1
+    )
 
     @GetMapping("/{id}")
     fun get(
