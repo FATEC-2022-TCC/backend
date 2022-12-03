56dfc1e73f133489a83a0ca37beaa1b508e60c3e
Donizete Vida <donizetevida1998@outlook.com>
2022-12-03 16:49:00 -0300

    Automatically finish a non successful adoption

--- src/main/kotlin/com/fatec/tcc/animais/adoption/domain/usecase/AddAdoptionStatusUseCase.kt
+++ src/main/kotlin/com/fatec/tcc/animais/adoption/domain/usecase/AddAdoptionStatusUseCase.kt
@@ -1,5 +1,6 @@
 package com.fatec.tcc.animais.adoption.domain.usecase
 
+import com.fatec.tcc.animais.adoption.domain.constant.AdoptionStatusEnum
 import com.fatec.tcc.animais.adoption.domain.constant.AdoptionStatusStateMachine
 import com.fatec.tcc.animais.adoption.domain.model.Adoption
 import com.fatec.tcc.animais.adoption.domain.model.UpdateAdoptionRequest
@@ -9,19 +10,27 @@ import com.fatec.tcc.animais.status.domain.model.Status
 
 @UseCase
 class AddAdoptionStatusUseCase(
-    private val repository: BaseRepository<Adoption>
+    private val repository: BaseRepository<Adoption>,
+    private val finishANonSuccessfulAdoptionUseCase: FinishANonSuccessfulAdoptionUseCase
 ) {
     operator fun invoke(
         request: UpdateAdoptionRequest
     ) = repository.find(request.id) notFoundOrElse {
         val currentStatus = AdoptionStatusStateMachine[currentStatusCode].notNullOrThrow()
         val desiredStatus = AdoptionStatusStateMachine[request.status.code].notNullOrThrow()
+
         AdoptionStatusStateMachine.isStateChangeAllowed(currentStatus, desiredStatus).trueOrThrow()
+
         request.status.run {
             Status(code, description, files.map(::Base64))
         }.also(statuses::add)
+
         copy(
             currentStatusCode = request.status.code
-        ).run(repository::update)
+        ).run(repository::update).run {
+            if (desiredStatus == AdoptionStatusEnum.IMPOSSIBLE) {
+                finishANonSuccessfulAdoptionUseCase(this)
+            }
+        }
     }
 }
---
+++ src/main/kotlin/com/fatec/tcc/animais/adoption/domain/usecase/FinishANonSuccessfulAdoptionUseCase.kt
@@ -0,0 +1,36 @@
+package com.fatec.tcc.animais.adoption.domain.usecase
+
+import com.fatec.tcc.animais.adoption.domain.constant.AdoptionRequestStatusEnum
+import com.fatec.tcc.animais.adoption.domain.model.Adoption
+import com.fatec.tcc.animais.base.BaseRepository
+import com.fatec.tcc.animais.base.UseCase
+import com.fatec.tcc.animais.status.domain.model.Status
+
+@UseCase
+class FinishANonSuccessfulAdoptionUseCase(
+    private val repository: BaseRepository<Adoption>
+) {
+    operator fun invoke(adoption: Adoption) {
+        adoption.apply {
+            val mappedRequests = requests.map {
+                if (it.currentStatusCode != AdoptionRequestStatusEnum.CANCELED.code) {
+                    it.apply {
+                        statuses.add(
+                            Status(
+                                AdoptionRequestStatusEnum.CANCELED.code,
+                                AdoptionRequestStatusEnum.CANCELED.description
+                            )
+                        )
+                    }.copy(
+                        currentStatusCode = AdoptionRequestStatusEnum.CANCELED.code
+                    )
+                } else {
+                    it
+                }
+            }
+            copy(
+                requests = mappedRequests as ArrayList
+            ).run(repository::update)
+        }
+    }
+}
