6217778db0d83cff96a183ddb9a66a40fbe967b8
Donizete Vida <donizetevida1998@outlook.com>
2022-12-10 05:06:35 -0300

    Remove unnecessary field

--- src/main/kotlin/com/fatec/tcc/animais/user/domain/model/UpdateUserRequest.kt
+++ src/main/kotlin/com/fatec/tcc/animais/user/domain/model/UpdateUserRequest.kt
@@ -3,7 +3,6 @@ package com.fatec.tcc.animais.user.domain.model
 data class UpdateUserRequest(
     val id: Long,
     val name: String,
-    val email: String,
     val authority: String,
     val isActive: Boolean,
     val isValidated: Boolean,
